5ef147b8981bef6b886f0abf9595024b37a78a86
benwestdev <32368718+benwestdev@users.noreply.github.com>
2021-09-08 13:36:44 -0400

    update to use webhistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
 import EventDetails from '@/views/EventDetails.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
